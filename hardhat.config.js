8b6d280c977c704b2fd3c4c629430d8bed922ba1
Ankush263 <ankushbanik263@gmail.com>
2022-07-24 16:42:12 +0530

    update

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,10 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+require("dotenv").config();
+
+const GOERLI_URL = process.env.GOERLI_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -6,4 +12,10 @@ module.exports = {
   paths: {
     artifacts: './frontend/src/artifacts',
   },
+  networks: {
+    goerli: {
+      url: GOERLI_URL,
+      accounts: [PRIVATE_KEY]
+    }
+  }
 };
